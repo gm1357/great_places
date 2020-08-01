c40038ea7799e143c0b7c785b1087bd4782eabe3
Gabriel Machado <g_machado1@hotmail.com>
2020-08-01 19:01:29 -0300

    Implementado salvamento de imagens

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,35 +1,25 @@
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 
 import '../models/place.dart';
 
-class GreatPlaces implements ChangeNotifier {
+class GreatPlaces with ChangeNotifier {
   List<Place> _items = [];
 
   List<Place> get items {
     return [..._items];
   }
 
-  @override
-  void addListener(listener) {
-    // TODO: implement addListener
-  }
+  void addPlace(String title, File pickedImage) {
+    final newPlace = Place(
+      id: DateTime.now().toString(),
+      image: pickedImage,
+      title: title,
+      location: null,
+    );
 
-  @override
-  void dispose() {
-    // TODO: implement dispose
-  }
-
-  @override
-  // TODO: implement hasListeners
-  bool get hasListeners => throw UnimplementedError();
-
-  @override
-  void notifyListeners() {
-    // TODO: implement notifyListeners
-  }
-
-  @override
-  void removeListener(listener) {
-    // TODO: implement removeListener
+    _items.add(newPlace);
+    notifyListeners();
   }
 }
--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,4 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/image_input.dart';
+import '../providers/great_places.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = 'add-place';
@@ -9,6 +15,22 @@ class AddPlaceScreen extends StatefulWidget {
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
   final _titleController = TextEditingController();
+  File _pickedImage;
+
+  void _selectImage(File pickedImage) {
+    _pickedImage = pickedImage;
+  }
+
+  void _savePlace() {
+    if (_titleController.text.isEmpty || _pickedImage == null) {
+      return;
+    }
+
+    Provider.of<GreatPlaces>(context, listen: false).addPlace(
+      _titleController.text,
+      _pickedImage,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +51,8 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                       decoration: InputDecoration(labelText: 'Title'),
                       controller: _titleController,
                     ),
+                    SizedBox(height: 10),
+                    ImageInput(_selectImage),
                   ],
                 ),
               ),
@@ -37,7 +61,7 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
           RaisedButton.icon(
             icon: Icon(Icons.add),
             label: Text('Add Place'),
-            onPressed: () {},
+            onPressed: _savePlace,
             elevation: 0,
             materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
             color: Theme.of(context).accentColor,
